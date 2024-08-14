e7affe5be31d049576a0637b5ef97bbd78674569
winter100 <100taeng@gmail.com>
2024-08-14 21:27:44 +0900

    fix: 기본 css 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
+
 import RootLayout from './components/common/layout/RootLayout';
+import Home from './pages/Home';
+import MatchingProfile from './pages/MatchingProfile';
+import Messages from './pages/Messages';
+import MatchLayout from './components/common/layout/MatchLayout';
+import Chatting from './pages/Chatting';
+import ChattingLayout from './components/common/layout/ChattingLayout';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+
+      {
+        path: 'match',
+        element: <MatchLayout />,
+        children: [
+          { index: true, element: <MatchingProfile /> },
+          {
+            path: 'messages',
+            element: <Messages />,
+          },
+          // { path: 'messages/:id', element: <Chatting /> },
+        ],
+      },
+      {
+        path: 'chatting/:id',
+        element: <ChattingLayout />,
+        children: [{ index: true, element: <Chatting /> }],
+      },
+    ],
   },
 ]);
 
