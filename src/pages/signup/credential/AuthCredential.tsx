a5856dd00dd9ca81cecd0dd4c6e51f477de48404
JHG <jhg990508@gmail.com>
2024-10-10 18:36:10 +0900

    feat: 회원가입 기능 업데이트
    아이디 input 추가, api 연결

--- src/pages/signup/credential/AuthCredential.tsx
+++ src/pages/signup/credential/AuthCredential.tsx
@@ -5,41 +5,100 @@ import { toast } from 'react-toastify';
 
 import styles from './authCredential.module.css';
 import MainButton from '../../../components/ui/MainButton';
+import { useState } from 'react';
 
 type Inputs = {
+  userName: string;
   email: string;
   password: string;
   confirmPassword: string;
+  certificationNumber: string;
 };
 
 const AuthCredential = () => {
+  const [emailVerification, setEmailVerification] = useState(false);
+  const [emailCheckingState, setEmailCheckingState] = useState(false);
   const nav = useNavigate();
   const { register, handleSubmit, watch } = useForm<Inputs>({
     mode: 'onSubmit',
     reValidateMode: 'onSubmit',
   });
+  const projectURL = import.meta.env.VITE_PROJECT_URL as string;
+  // const projectURL = import.meta.env.VITE_PROJECT_SERVER_URL as string;
 
+  //입력된 유저정보
+  const userName = watch('userName');
+  const email = watch('email');
   const password = watch('password');
+  const certificationNumber = watch('certificationNumber');
 
+  //회원가입 함수
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    const projectUrl = import.meta.env.VITE_PROJECT_URL as string;
-    const res = await axios.post(`${projectUrl}/api/signup`, {
-      data,
-    });
-    console.log(res);
-    nav('/signup/setting/gender');
+    try {
+      const response = await axios.post(`${projectURL}/api/signup`, {
+        username: data.userName,
+        password: data.password,
+        email: data.email,
+      });
+      console.log(response.data);
+      nav('/signup/setting/gender');
+    } catch (error) {
+      console.error('signupError', error);
+    }
   };
 
-  const onError = (error: FieldErrors<Inputs>) => {
-    if (error.email) {
-      toast.error(error.email.message);
-      return null;
+  //회원가입 에러 함수
+  const onError = (errors: FieldErrors<Inputs>) => {
+    const errorMessages = [
+      'userName',
+      'email',
+      'password',
+      'confirmPassword',
+    ] as const;
+
+    // 첫 번째로 발견된 에러만 처리 (선택적)
+    for (const field of errorMessages) {
+      if (errors[field]) {
+        toast.error(errors[field]?.message);
+        return;
+      }
     }
-    if (error.password) {
-      toast.error(error.password.message);
-      return null;
+  };
+
+  //인증번호 요청 함수
+  const getVerificationBtnHanddler = async () => {
+    try {
+      const response = await axios.post(
+        `${projectURL}/api/v1/auth/email-certification`,
+        {
+          username: userName,
+          email,
+        }
+      );
+
+      console.log(response.data);
+      setEmailCheckingState(!emailCheckingState);
+    } catch (error) {
+      console.error('error', error);
     }
-    error.confirmPassword && toast.error(error.confirmPassword.message);
+  };
+
+  //인증번호 확인 함수
+  const postVerificationNumBtnHanddler = async () => {
+    try {
+      const response = await axios.post(
+        `${projectURL}/api/v1/auth/check-certification`,
+        {
+          id: userName,
+          email,
+          certificationNumber,
+        }
+      );
+      console.log(response.data);
+      setEmailVerification(!emailVerification);
+    } catch (error) {
+      console.error('error', error);
+    }
   };
   return (
     <>
@@ -50,22 +109,90 @@ const AuthCredential = () => {
         <form onSubmit={handleSubmit(onSubmit, onError)}>
           <div className={styles.inputContainer}>
             <div>
-              <label htmlFor="email">이메일</label>
+              <label htmlFor="userName">아이디</label>
               <input
-                className={styles.emailInput}
-                {...register('email', {
-                  required: '이메일을 입력해주세요',
+                type="text"
+                readOnly={emailCheckingState}
+                style={{
+                  backgroundColor: emailCheckingState ? '#4c4c4c' : 'black',
+                }}
+                autoComplete="off"
+                {...register('userName', {
+                  required: '아이디를 입력해주세요.',
                   pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: '이메일 형식이 올바르지 않습니다.',
+                    value: /^[a-zA-Z0-9]+$/,
+                    message: '아이디는 영어와 숫자만 사용할 수 있습니다.',
+                  },
+                  minLength: {
+                    value: 6,
+                    message: '아이디는 최소 6자 이상이어야 합니다.',
+                  },
+                  maxLength: {
+                    value: 12,
+                    message: '아이디는 최대 12자까지 입력할 수 있습니다.',
                   },
                 })}
               />
             </div>
             <div>
+              <label htmlFor="email">이메일</label>
+              <div className={styles.inputBtnStack}>
+                <input
+                  className={styles.emailInput}
+                  readOnly={emailCheckingState}
+                  autoComplete="off"
+                  style={{
+                    backgroundColor: emailCheckingState ? '#4c4c4c' : 'black',
+                  }}
+                  {...register('email', {
+                    required: '이메일을 입력해주세요.',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: '이메일 형식이 올바르지 않습니다.',
+                    },
+                  })}
+                />
+
+                <button
+                  type="button"
+                  onClick={getVerificationBtnHanddler}
+                  disabled={emailCheckingState}
+                >
+                  인증번호 받기
+                </button>
+              </div>
+            </div>
+            {emailCheckingState && (
+              <div>
+                <label htmlFor="verificationInput">인증번호 입력</label>
+                <div className={styles.inputBtnStack}>
+                  <input
+                    type="text"
+                    id="verificationInput"
+                    readOnly={emailVerification}
+                    style={{
+                      backgroundColor: emailVerification ? '#4c4c4c' : 'black',
+                    }}
+                    {...register('certificationNumber')}
+                  />
+                  <button
+                    type="button"
+                    onClick={postVerificationNumBtnHanddler}
+                    disabled={emailVerification}
+                  >
+                    인증하기
+                  </button>
+                </div>
+              </div>
+            )}
+            <div>
               <label htmlFor="password">비밀번호</label>
               <input
                 type="password"
+                disabled={!emailVerification}
+                style={{
+                  backgroundColor: !emailVerification ? '#4c4c4c' : undefined,
+                }}
                 {...register('password', {
                   required: '비밀번호를 입력해주세요.',
                   minLength: {
@@ -94,6 +221,10 @@ const AuthCredential = () => {
               <label htmlFor="confirmPassword">비밀번호 확인</label>
               <input
                 type="password"
+                disabled={!emailVerification}
+                style={{
+                  backgroundColor: emailVerification ? 'black' : '#4c4c4c',
+                }}
                 {...register('confirmPassword', {
                   required: '비밀번호를 확인해주세요.',
                   validate: (value) =>
@@ -103,7 +234,11 @@ const AuthCredential = () => {
             </div>
           </div>
           <div className={styles.btnWrapper}>
-            <MainButton text="가입하기" type="submit" />
+            <MainButton
+              text="가입하기"
+              type="submit"
+              disabled={!emailVerification}
+            />
           </div>
         </form>
       </div>
