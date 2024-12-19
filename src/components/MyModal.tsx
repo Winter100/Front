8cec808ffb98d0649fa386d6f0467261897de0b1
winter100 <100taeng@gmail.com>
2024-12-19 14:39:48 +0900

    feat: 광고 이미지 추가 및 모달 수정

--- src/components/MyModal.tsx
+++ src/components/MyModal.tsx
@@ -2,15 +2,15 @@ import { useRef, useState } from 'react';
 import Modal from 'react-modal';
 import Button from './common/Button';
 import styles from './MyModal.module.css';
-import UserImage from './common/UserImage';
 import instance from '../api/axios';
 import { useQueryClient } from '@tanstack/react-query';
 import { getAccessToken } from '../util/token';
+import { CgProfile } from 'react-icons/cg';
+import Spinner from './common/Spinner';
 
 type MyProfile = {
   profileName: string | undefined;
   selfIntroduction: string | undefined;
-  profileImages: string | undefined;
 };
 const overlay = {
   backgroundColor: ' rgba(0, 0, 0, 0.7)',
@@ -23,25 +23,23 @@ const content = {
   bottom: 'auto',
   marginRight: '-50%',
   transform: 'translate(-50%, -50%)',
-  width: '400px',
-  height: '450px',
+  width: '350px',
+  height: '250px',
   zIndex: 100,
   backgroundColor: '#202123',
   border: 'none',
 };
 
-const MyModal = ({
-  profileName = '',
-  selfIntroduction = '',
-  profileImages = '/profile.png',
-}: MyProfile) => {
+const MyModal = ({ profileName = '', selfIntroduction = '' }: MyProfile) => {
   const [openModal, setOpenModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const profileNameRef = useRef<HTMLInputElement>(null);
   const selfIntroductionRef = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
     const token = getAccessToken();
 
     const profileName = profileNameRef.current?.value;
@@ -74,63 +72,73 @@ const MyModal = ({
       setOpenModal(false);
     } catch (error) {
       console.error('API 요청 중 오류 발생:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div>
-      <Button
-        onClick={() => setOpenModal((pre) => !pre)}
-        style={{ fontSize: '0.8rem' }}
-      >
-        프로필 수정
+      <Button onClick={() => setOpenModal((pre) => !pre)}>
+        <CgProfile />
       </Button>
-      <Modal
-        ariaHideApp={false}
-        isOpen={openModal}
-        onRequestClose={() => setOpenModal(false)}
-        style={{ overlay, content }}
-        contentLabel="마이페이지 모달"
-      >
-        <div className={styles.container}>
-          <div className={styles.image_container}>
-            <UserImage src={profileImages ?? '/profile.png'} size="MODAL" />
+      {openModal && (
+        <Modal
+          ariaHideApp={false}
+          isOpen={openModal}
+          onRequestClose={() => setOpenModal(false)}
+          style={{ overlay, content }}
+          contentLabel="마이페이지 모달"
+        >
+          <div className={styles.container}>
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <div className={styles.input_container}>
+                <label htmlFor="profileName" className={styles.title}>
+                  이름
+                </label>
+                <input
+                  type="text"
+                  id="profileName"
+                  className={styles.input}
+                  ref={profileNameRef}
+                  defaultValue={profileName}
+                  name="profileName"
+                />
+              </div>
+
+              <div className={styles.input_container}>
+                <label htmlFor="selfIntroduction" className={styles.title}>
+                  자기소개
+                </label>
+                <input
+                  type="text"
+                  id="selfIntroduction"
+                  className={styles.input}
+                  ref={selfIntroductionRef}
+                  defaultValue={selfIntroduction}
+                  name="selfIntroduction"
+                />
+              </div>
+
+              <div className={styles.btn_container}>
+                <button
+                  disabled={isLoading}
+                  style={{
+                    width: '5rem',
+                    height: '2rem',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  className={styles.btn}
+                >
+                  {isLoading ? <Spinner size={20} /> : '수정하기'}
+                </button>
+              </div>
+            </form>
           </div>
-
-          <form className={styles.form} onSubmit={handleSubmit}>
-            <div className={styles.input_container}>
-              <label htmlFor="profileName" className={styles.title}>
-                이름
-              </label>
-              <input
-                type="text"
-                id="profileName"
-                className={styles.input}
-                ref={profileNameRef}
-                defaultValue={profileName}
-                name="profileName"
-              />
-            </div>
-
-            <div className={styles.input_container}>
-              <label htmlFor="selfIntroduction" className={styles.title}>
-                자기소개
-              </label>
-              <input
-                type="text"
-                id="selfIntroduction"
-                className={styles.input}
-                ref={selfIntroductionRef}
-                defaultValue={selfIntroduction}
-                name="selfIntroduction"
-              />
-            </div>
-            <div className={styles.btn_container}>
-              <button className={styles.btn}>수정</button>
-            </div>
-          </form>
-        </div>
-      </Modal>
+        </Modal>
+      )}
     </div>
   );
 };
